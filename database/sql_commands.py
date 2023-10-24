d00ebbf74b454f79168b99f10b0eb4efcea256fe
Choposh911 <choposha09@gmail.com>
2023-10-24 10:30:07 +0300

    HW4

--- database/sql_commands.py
+++ database/sql_commands.py
@@ -3,86 +3,124 @@ from database import sql_queries
 
 
 class Database:
-    def __init__(self):
-        self.connection = sqlite3.connect("db.sqlite3")
-        self.cursor = self.connection.cursor()
+  def __init__(self):
+    self.connection = sqlite3.connect("db.sqlite3")
+    self.cursor = self.connection.cursor()
 
-    def sql_create_tables(self):
-        if self.connection:
-            print("Database connected successfully")
+  def sql_create_tables(self):
+    if self.connection:
+      print("Database connected successfully")
 
-        self.connection.execute(sql_queries.CREATE_USER_TABLE_QUERY)
-        self.connection.execute(sql_queries.CREATE_BAN_USER_TABLE_QUERY)
-        self.connection.execute(sql_queries.CREATE_USER_FORM_TABLE_QUERY)
+    self.connection.execute(sql_queries.CREATE_USER_TABLE_QUERY)
+    self.connection.execute(sql_queries.CREATE_BAN_USER_TABLE_QUERY)
+    self.connection.execute(sql_queries.CREATE_USER_FORM_TABLE_QUERY)
+    self.connection.execute(sql_queries.CREATE_LIKE_TABLE_QUERY)
 
-        self.connection.commit()
+    self.connection.commit()
 
-    def sql_insert_user_query(self, telegram_id, username, first_name, last_name):
-        self.cursor.execute(
-            sql_queries.INSERT_USER_QUERY,
-            (None, telegram_id, username, first_name, last_name,)
-        )
-        self.connection.commit()
+  def sql_insert_user_query(self, telegram_id, username, first_name, last_name):
+    self.cursor.execute(
+      sql_queries.INSERT_USER_QUERY,
+      (None, telegram_id, username, first_name, last_name,)
+    )
+    self.connection.commit()
 
-    def sql_select_all_user_query(self):
-        self.cursor.row_factory = lambda cursor, row: {
-            'id': row[0],
-            'telegram_id': row[1],
-            'username': row[2],
-            'first_name': row[3],
-            'last_name': row[4],
-        }
-        return self.cursor.execute(
-            sql_queries.SELECT_ALL_USER_QUERY
-        ).fetchall()
-        # self.connection.commit()
+  def sql_select_all_user_query(self):
+    self.cursor.row_factory = lambda cursor, row: {
+      'id': row[0],
+      'telegram_id': row[1],
+      'username': row[2],
+      'first_name': row[3],
+      'last_name': row[4],
+    }
+    return self.cursor.execute(
+      sql_queries.SELECT_ALL_USER_QUERY
+    ).fetchall()
+    # self.connection.commit()
 
-    def sql_insert_ban_user_query(self, telegram_id, username):
-        self.cursor.execute(
-            sql_queries.INSERT_BAN_USER_QUERY,
-            (None, telegram_id, username, 1)
-        )
-        self.connection.commit()
+  def sql_insert_ban_user_query(self, telegram_id, username):
+    self.cursor.execute(
+      sql_queries.INSERT_BAN_USER_QUERY,
+      (None, telegram_id, username, 1)
+    )
+    self.connection.commit()
 
-    def sql_update_ban_user_query(self, telegram_id):
-        self.cursor.execute(
-            sql_queries.UPDATE_BAN_USER_COUNT_QUERY,
-            (telegram_id,)
-        )
-        self.connection.commit()
+  def sql_update_ban_user_query(self, telegram_id):
+    self.cursor.execute(
+      sql_queries.UPDATE_BAN_USER_COUNT_QUERY,
+      (telegram_id,)
+    )
+    self.connection.commit()
 
-    def sql_select_user_query(self, telegram_id):
-        self.cursor.row_factory = lambda cursor, row: {
-            'id': row[0],
-            'telegram_id': row[1],
-            'username': row[2],
-            'first_name': row[3],
-            'last_name': row[4],
-        }
-        return self.cursor.execute(
-            sql_queries.SELECT_USER_QUERY,
-            (telegram_id,)
-        ).fetchall()
 
-    def sql_insert_user_form_query(self, telegram_id, nickname, bio,
-                                   age, occupation, photo):
-        self.cursor.execute(
-            sql_queries.INSERT_USER_FORM_QUERY,
-            (None, telegram_id, nickname, bio, age, occupation, photo,)
-        )
-        self.connection.commit()
+  def sql_select_user_query(self, telegram_id):
+    self.cursor.row_factory = lambda cursor, row: {
+      'id': row[0],
+      'telegram_id': row[1],
+      'username': row[2],
+      'first_name': row[3],
+      'last_name': row[4],
+    }
+    return self.cursor.execute(
+      sql_queries.SELECT_USER_QUERY,
+      (telegram_id,)
+    ).fetchall()
 
-    def sql_select_user_form_query(self, telegram_id):
-        self.cursor.row_factory = lambda cursor, row: {
-            'id': row[0],
-            'telegram_id': row[1],
-            'nickname': row[2],
-            'bio': row[3],
-            'age': row[4],
-            'occupation': row[5],
-            'photo': row[6],
-        }
-        return self.cursor.execute(
-            sql_queries.SELECT_USER_FORM_QUERY,
-            (telegram_id,)
-        ).fetchall()
+
+  def sql_insert_user_form_query(self, telegram_id, nickname, bio, age, occupation, photo):
+    self.cursor.execute(
+      sql_queries.INSERT_USER_FORM_QUERY,
+      (None, telegram_id, nickname, bio, age, occupation, photo,)
+    )
+    self.connection.commit()
+
+
+  def sql_select_user_form_query(self, telegram_id):
+    self.cursor.row_factory = lambda cursor, row: {
+      'id': row[0],
+      'telegram_id': row[1],
+      'nickname': row[2],
+      'bio': row[3],
+      'age': row[4],
+      'occupation': row[5],
+      'photo': row[6],
+    }
+    return self.cursor.execute(
+      sql_queries.SELECT_USER_FORM_QUERY,
+      (telegram_id,)
+    ).fetchall()
+
+  def sql_select_all_users_form_query(self):
+    self.cursor.row_factory = lambda cursor, row: {
+      'id': row[0],
+      'telegram_id': row[1],
+      'nickname': row[2],
+      'bio': row[3],
+      'age': row[4],
+      'occupation': row[5],
+      'photo': row[6],
+    }
+    return self.cursor.execute(
+      sql_queries.SELECT_ALL_USERS_FORM_QUERY,
+    ).fetchall()
+
+  def sql_insert_like_query(self, owner, liker):
+    self.cursor.execute(
+      sql_queries.INSERT_LIKE_QUERY,
+      (None, owner, liker,)
+    )
+    self.connection.commit()
+
+  def sql_delete_form_query(self, owner):
+    self.cursor.execute(
+      sql_queries.DELETE_USER_FORM_QUERY,
+      (owner, )
+    )
+    self.connection.commit()
+
+  def sql_update_form_query(self,  nickname, bio, age, occupation, photo, telegram_id):
+    self.cursor.execute(
+      sql_queries.UPDATE_USER_FORM_QUERY,
+      ( nickname, bio, age, occupation, photo, telegram_id , )
+    )
+    self.connection.commit()
--- keyboards/inline_buttons.py
+++ keyboards/inline_buttons.py
@@ -2,45 +2,87 @@ from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 
 async def start_keyboard():
-    markup = InlineKeyboardMarkup()
-    quastionaire_button = InlineKeyboardButton(
-        "Start Questionaire",
-        callback_data="start_quesionnaire"
-    )
-    registration_button = InlineKeyboardButton(
-        "registration",
-        callback_data="fsm_start"
-    )
-    my_profile_button = InlineKeyboardButton(
-        "my profile",
-        callback_data="my_profile"
-    )
-    markup.add(quastionaire_button)
-    markup.add(registration_button)
-    markup.add(my_profile_button)
-    return markup
+  markup = InlineKeyboardMarkup()
+  quastionaire_button = InlineKeyboardButton(
+    "Start Questionaire",
+    callback_data="start_quesionnaire"
+  )
+  registration_button = InlineKeyboardButton(
+    "registration",
+    callback_data="fsm_start"
+  )
+  my_profile_button = InlineKeyboardButton(
+    "my profile",
+    callback_data="my_profile"
+  )
+  view_profiles_button = InlineKeyboardButton(
+    "view profile",
+    callback_data="random_profile"
+  )
+  markup.add(quastionaire_button)
+  markup.add(registration_button)
+  markup.add(my_profile_button)
+  markup.add(view_profiles_button)
+  return markup
 
 
 async def quesionnaire_one_keybords():
-    markup = InlineKeyboardMarkup()
-    yes_button = InlineKeyboardButton(
-        "yes",
-        callback_data="yes_trip"
-    )
-    no_button = InlineKeyboardButton(
-        "no",
-        callback_data="no_trip"
-    )
-    markup.add(yes_button)
-    markup.add(no_button)
-    return markup
+  markup = InlineKeyboardMarkup()
+  yes_button = InlineKeyboardButton(
+    "yes",
+    callback_data="yes_trip"
+  )
+  no_button = InlineKeyboardButton(
+    "no",
+    callback_data="no_trip"
+  )
+  markup.add(yes_button)
+  markup.add(no_button)
+  return markup
 
 
 async def admin_keybords():
-    markup = InlineKeyboardMarkup()
-    admin_user_list_button = InlineKeyboardButton(
-        "user list",
-        callback_data="admin_user_list"
-    )
-    markup.add(admin_user_list_button)
-    return markup
+  markup = InlineKeyboardMarkup()
+  admin_user_list_button = InlineKeyboardButton(
+    "user list",
+    callback_data="admin_user_list"
+  )
+  markup.add(admin_user_list_button)
+  return markup
+
+async def like_dislike_keyboard(owner_tg_id):
+  markup = InlineKeyboardMarkup()
+  user_form_like_button = InlineKeyboardButton(
+    "like",
+    callback_data=f"user_form_like{owner_tg_id}"
+  )
+  user_form_dislike_button = InlineKeyboardButton(
+    "dislike",
+    callback_data="random_profile"
+  )
+  markup.add(user_form_like_button)
+  markup.add(user_form_dislike_button)
+  return markup
+
+async def edit_delete_keyboard():
+  markup = InlineKeyboardMarkup()
+  edit_form_button = InlineKeyboardButton(
+    "update",
+    callback_data="fsm_start"
+  )
+  delete_form_button = InlineKeyboardButton(
+    "delete",
+    callback_data="delete_profile"
+  )
+  markup.add(edit_form_button)
+  markup.add(delete_form_button)
+  return markup
+
+async def my_profile_register():
+  markup = InlineKeyboardMarkup()
+  registation_button = InlineKeyboardButton(
+    "registration",
+    callback_data="fsm_start"
+  )
+  markup.add(registation_button)
+  return markup
